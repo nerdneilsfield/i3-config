85d4396efbebc6ed8821a032abe42790c54313b7
Jiang 澤民 <dengqi935@gmail.com>
2018-03-04 07:28:18 +0800

    Add some new feature

--- bin/wallpaper.sh
+++ bin/wallpaper.sh
@@ -3,7 +3,7 @@
 TIMEOUT=3600
 
 while [ true ]; do
-  if ping -q -c 1 -W 1 8.8.8.8 >/dev/null; then
+  if ping -q -c 1 -W 1 baidu.com >/dev/null; then
     url="http://www.bing.com"$(http "http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=50" | jq -r '.images[].url' | shuf -n1)
     name=$(basename "$url")
     cd ~/.bing-wallpapers && wget -q "$url"
